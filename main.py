85fe698dc8b7acf483e60fc440cc881223875737
Cerecero <ale.rey.cer@outlook.com>
2024-02-05 13:12:20 -0600

    changed to code to add sprite.Groups

--- main.py
+++ main.py
@@ -5,31 +5,47 @@ from player import Player
 
 def main():
     pygame.init()
-    # print("Starting asteroid!")
-    # print(SCREEN_WIDTH)
-    # print(SCREEN_HEIGHT)
+
+    # set the window resolution
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     delta_time = pygame.time.Clock()
     dt = 0
+    
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    Player.containers = (updatable, drawable)
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # draw the player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # print(f"This is drawable { drawable }")
+    # print(f"This is updatable: {updatable}")
+
+
+    # drawable = pygame.sprite.Group()  # all objects that can be drawn
+    
     while True:
         for event in pygame.event.get():
+            # Quit window
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, color="black")
-        player.draw(screen) # draw here, anywhere else the screen renders the background above the player sprite
-        player.update(dt)
+        # draw here, anywhere else the screen renders the background above the player sprite
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
+        # player.draw(screen)
+        # player.update(dt)
+
         pygame.display.flip()
-
-
 
         delta_time.tick(60)
         dt = delta_time.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
