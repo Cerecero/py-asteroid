10c3faf9f4cd2e00b14f9d1caceaa8aba01dc143
Cerecero <ale.rey.cer@outlook.com>
2024-02-08 12:02:42 -0600

    added a player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,16 +11,24 @@ def main():
     delta_time = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # pygame.Surface.fill(screen, color="black")
-        # pygame.display.flip()
+        pygame.Surface.fill(screen, color="black")
+        player.draw(screen) # draw here, anywhere else the screen renders the background above the player sprite
+        pygame.display.flip()
 
-        # delta_time.tick(60)
-        # dt = delta_time.tick(60) / 1000
+
+        delta_time.tick(60)
+        dt = delta_time.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
