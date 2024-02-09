a297f25a0e37c1e5c4fb1e5f4be3bd2135408631
Cerecero <ale.rey.cer@outlook.com>
2024-02-09 14:20:03 -0600

    added delta_time

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroid!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    # print("Starting asteroid!")
+    # print(SCREEN_WIDTH)
+    # print(SCREEN_HEIGHT)
+    delta_time = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
-        pygame.display.flip()
+        # pygame.Surface.fill(screen, color="black")
+        # pygame.display.flip()
+
+        # delta_time.tick(60)
+        # dt = delta_time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
